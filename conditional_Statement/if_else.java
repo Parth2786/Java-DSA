b70165951b1fecf114749d15170416620d05ead9
Parth <vishwakarmaparth20@gmail.com>
2026-05-22 16:27:15 +0530

    Update if_else.java

--- conditional_Statement/if_else.java
+++ conditional_Statement/if_else.java
@@ -4,10 +4,10 @@ public class if_else {
     public static void main(String[] args) {
         int age = 18;
         if (age >= 18) {
-            System.out.println("Eligible");
+            System.out.println("You are Eligible for voting");
         }
         else{
-            System.out.println("Not Eligible");
+            System.out.println("You are not Eligible for voting");
         }
     }
 }
