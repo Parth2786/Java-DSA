183b7fb2d4cb43c2932fd9336d8a63aebfd2e733
Parth <vishwakarmaparth20@gmail.com>
2026-05-22 16:23:40 +0530

    if_else

---
+++ conditional_Statement/if_else.java
@@ -0,0 +1,13 @@
+package conditional_Statement;
+
+public class if_else {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Eligible");
+        }
+        else{
+            System.out.println("Not Eligible");
+        }
+    }
+}
