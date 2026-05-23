b99a2b7b66befa8ec8f663febb8241395683b43b
Parth <vishwakarmaparth20@gmail.com>
2026-05-23 09:48:20 +0530

    Greet Function

---
+++ Functions/question7.java
@@ -0,0 +1,11 @@
+package Functions;
+
+public class question7 {
+    public static String greet(String name){
+        return "Hello "+name;
+    }
+    public static void main(String[] args) {
+        String name = "Parth";
+        System.out.println(greet(name));
+    }
+}
