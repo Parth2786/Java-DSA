41ac33b46bdce0324eac4e37062b375a2a08513d
Parth <vishwakarmaparth20@gmail.com>
2026-05-23 08:17:21 +0530

    Cross Pattern

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,19 @@
+package Patterns;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == i || i + j == n - 1 ) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print(" ");
+                }
+                    
+            }
+            System.out.println();
+        }
+    }
+}
