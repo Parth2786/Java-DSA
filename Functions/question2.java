6d41e1c9272ecf9a0fc13215f509db394efef1c6
Parth <vishwakarmaparth20@gmail.com>
2026-05-23 09:00:49 +0530

    Addition Function

---
+++ Functions/question2.java
@@ -0,0 +1,17 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class question2 {
+    public static int add(int a, int b, int c){
+        int sum = a+ b + c;
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(add(a, b, c));
+    }
+}
