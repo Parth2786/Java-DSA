eed47b07f7563781c52a10342bf63375a69c0ceb
Parth <vishwakarmaparth20@gmail.com>
2026-05-23 14:18:40 +0530

    Binomial Coefficient

--- Functions/question8.java
+++ Functions/question8.java
@@ -3,22 +3,15 @@ package Functions;
 import java.util.Scanner;
 
 public class question8 {
-    public static int binomial_coefficient(int n, int r){
-        int fact_numerator = 1;
-        int fact_dinominator_1 = 1;
-        int fact_dinominator_2 = 1;
-        for (int i = 1; i <= n; i++) {
-            fact_numerator = fact_numerator * i;
-        }
-        for (int i = 1; i <= n - r ; i++) {
-            fact_dinominator_1 *= i;
-        }
+    public static int binomial_coefficient(int n, int r) {
+        int result = 1;
         for (int i = 1; i <= r; i++) {
-            fact_dinominator_2 *= i;
+            result = result * (n - i + 1) / i;
         }
-        int Binomial_coefficient = fact_numerator/(fact_dinominator_1*fact_dinominator_2);
-        return Binomial_coefficient;
+        return result;
+
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
