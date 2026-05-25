c228b2fcedb789ad7313d988de4536a6995e9b39
Parth <vishwakarmaparth20@gmail.com>
2026-05-25 08:39:34 +0530

    Sum of Even Number Function.

---
+++ Functions/question12.java
@@ -0,0 +1,18 @@
+package Functions;
+
+public class question12 {
+    public static int func(int start, int end){
+        int sum = 0;
+        for (int i = start; i < end; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int start = 1;
+        int end = 10;
+        System.out.println(func(start, end));
+    }
+}
--- Patterns/Pattern10.java
+++ Patterns/Pattern10.java
@@ -11,6 +11,6 @@ public class Pattern10 {
                 System.out.print("* ");
             }
             System.out.println("");
-        }
+        }  
     }
 }
