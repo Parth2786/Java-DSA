3cc100e49149c869fcf4c507cb0c21a9c151ba2d
Parth <vishwakarmaparth20@gmail.com>
2026-05-26 12:09:38 +0530

    Binary to Decimal

---
+++ Functions/question17.java
@@ -0,0 +1,20 @@
+package Functions;
+
+public class question17 {
+    public static int Binary_Decimal(int n){
+        int result = 0;
+        int place = 1;  
+        while (n > 0) {
+            int digit = n % 10;          
+            result = result + digit * place; 
+            place *= 2;                  
+            n /= 10;                     
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int n = 11001;
+        System.out.println(Binary_Decimal(n));
+
+    }
+}
