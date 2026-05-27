788dc4fcc73010aa4fbf67b7786e575160c932d0
Parth <vishwakarmaparth20@gmail.com>
2026-05-27 09:49:37 +0530

    Maximum Number

---
+++ Arrays/question5.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class question5 {
+    public static int maximum_number(int[] arr){
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(maximum_number(arr));
+    }
+}
