3fb6cb0c83e2669318ecc8504cdd30b19da18c5d
Parth <vishwakarmaparth20@gmail.com>
2026-05-27 09:00:48 +0530

    Even Numbers

---
+++ Arrays/question2.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The even number from the array are : ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
