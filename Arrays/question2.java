110ac84eafc1c303cfe5a45492ed50095156e4a6
Parth <vishwakarmaparth20@gmail.com>
2026-05-27 09:07:55 +0530

    Update question2.java

--- Arrays/question2.java
+++ Arrays/question2.java
@@ -3,6 +3,16 @@ package Arrays;
 import java.util.Scanner;
 
 public class question2 {
+    public static void even_number(int [] arr){
+        
+        System.out.println("The even number from the array are : ");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i]+" ");
+            }
+        }
+    
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -10,11 +20,7 @@ public class question2 {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("The even number from the array are : ");
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0) {
-                System.out.print(arr[i]+" ");
-            }
-        }
+        even_number(arr);
+
     }
 }
