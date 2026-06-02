d675645539cf5bef52c1196e9c490a031d3e52dc
Parth <vishwakarmaparth20@gmail.com>
2026-06-02 12:12:58 +0530

    All subarray of Array

---
+++ Arrays/question19.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class question19 {
+    public static void subarray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        subarray(arr);
+    }
+}
