cb9d7048cdce35cd26286fd94748c095b9f54f6e
Parth <vishwakarmaparth20@gmail.com>
2026-06-23 10:49:22 +0530

    Transpose of Matrix

---
+++ multi_Dim_Array/question2.java
@@ -0,0 +1,31 @@
+package multi_Dim_Array;
+
+public class question2 {
+    public static int[][] transpose(int[][] matrix){
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] result = new int[cols][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        int res [][] = transpose(matrix);
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res.length; j++) {
+                System.out.print(res[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
