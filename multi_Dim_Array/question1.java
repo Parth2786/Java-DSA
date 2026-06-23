4ea8bc8d0449d466c62cef67526bb616ee8ca1cf
Parth <vishwakarmaparth20@gmail.com>
2026-06-23 10:43:27 +0530

    Traversing 2D Array

---
+++ multi_Dim_Array/question1.java
@@ -0,0 +1,19 @@
+package multi_Dim_Array;
+
+public class question1 {
+    public static void printmat(int[][] mat){
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print(mat[i][j]+" ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int mat[][] = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12}
+        };
+        printmat(mat);
+    }
+}
