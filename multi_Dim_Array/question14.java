cd7ed29bc3d7c2810e0ea0dd1c59bca57a77ddd3
Parth <vishwakarmaparth20@gmail.com>
2026-07-07 13:14:25 +0530

    Leetcode 74

---
+++ multi_Dim_Array/question14.java
@@ -0,0 +1,23 @@
+package multi_Dim_Array;
+
+public class question14 {
+    public static boolean search_matrix(int[][] matrix, int target){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (target == matrix[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1,3,5,7},
+            {10,11,16,20},
+            {23,30,34,60}
+        };
+        int target = 11;
+        System.out.println(search_matrix(matrix,target));
+    }
+}
