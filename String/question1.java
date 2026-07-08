44133090fc64fd4caaa437ac0296573d4103a085
Parth <vishwakarmaparth20@gmail.com>
2026-07-08 09:25:12 +0530

    Count words in String

---
+++ String/question1.java
@@ -0,0 +1,23 @@
+package String;
+import java.util.*;
+public class question1 {
+    public static int count_word(String str){
+        int count = 0;
+        boolean inword = false;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '\t' || c == '\n' || c == ' ') {
+                inword = false;
+            }
+            else if (!inword) {
+                count++;
+                inword = true;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        String str = "cat dog meow";
+        System.out.println(count_word(str));
+    }
+}
