d827cf577862d9cf542a0fc6740b0aaf57ea2558
Parth <vishwakarmaparth20@gmail.com>
2026-07-09 10:09:05 +0530

    Leetcode 242

---
+++ String/question7.java
@@ -0,0 +1,23 @@
+package String;
+
+public class question7 {
+    public static boolean isAnagram(String s, String t){
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+                count[t.charAt(i) - 'a']--;
+            
+        }
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s,t));
+    }
+}
