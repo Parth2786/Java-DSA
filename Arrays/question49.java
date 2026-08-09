d0ed82c31a3994557d89f8caeae1cf446945714a
Parth <vishwakarmaparth20@gmail.com>
2026-08-09 14:58:34 +0530

    Leetcode 217

---
+++ Arrays/question49.java
@@ -0,0 +1,17 @@
+package Arrays;
+import java.util.*;
+public class question49 {
+    public static boolean contains_duplicate(int[] nums){
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(contains_duplicate(nums));
+    }
+}
